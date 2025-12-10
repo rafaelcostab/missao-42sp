17f5b059444f4cb3b16c420da34851bd1cf7d89c
Rafael Costa Bezerra <rcosta-b@c2r10p6.42sp.org.br>
2025-12-10 12:42:08 -0300

    feat: manipulating arrays

--- modulo5/ex01/play_with_arrays.py
+++ modulo5/ex01/play_with_arrays.py
@@ -4,8 +4,7 @@ array = [2, 8, 9, 48, 8, 22, -12, 2]
 array_new = []
 
 for i in range(len(array)):
-    array_new[i] = array[i] + 2
-
+    array_new.append(array[i] + 2)
 
 print("Original array: " + str(array))
 print("New array: " + str(array_new))
