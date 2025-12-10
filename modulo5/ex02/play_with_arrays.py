761281b0ae3a26c43194d9a0b8299d0e3d08e3f1
Rafael Costa Bezerra <rcosta-b@c2r10p6.42sp.org.br>
2025-12-10 12:51:14 -0300

    feat: manipulating arrays, once again

---
+++ modulo5/ex02/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+array_new = []
+
+for i in range(len(array)):
+    if array[i] > 5:
+        array_new.append(array[i] + 2)
+
+print(str(array))
+print(str(array_new))
